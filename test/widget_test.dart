ce3ee07c5d2e1484734caf40fe8833ef3c56a182
Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2019-04-16 05:33:32 +0000

    Updated widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,19 +8,11 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mextv_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(WelcomeScreen());
-    // Create our Finders
-    final titleFinder = find.text('Home');
 
-
-    // Use the `findsOneWidget` matcher provided by flutter_test to verify our
-    // Text Widgets appear exactly once in the Widget tree
-    expect(titleFinder, findsOneWidget);
 
   });
 }
